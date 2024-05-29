9bfa2e1f525d15d807c7a4921f5638e991ffda3f
Arif <arifusmani154@gmail.com>
2024-05-29 17:00:26 +0530

    first commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TransformationPage = (props: Props) => {
+  return (
+    <div>TransformationPage</div>
+  )
+}
+
+export default TransformationPage
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddTransformationPage = () => {
+  return (
+    <div>
+      AddTransformationPage
+AddTransformationPage
+    </div>
+  )
+}
+
+export default AddTransformationPage
